333c74cb230406e42a175d82b19d99c41bd6491f
Enes <98012557+enesaks@users.noreply.github.com>
2024-04-15 20:04:44 +0300

    Sql Tabloları oluşturuldu.

---
+++ CsharpMyPortfolio/DAL/Entities/Skill.cs
@@ -0,0 +1,9 @@
+﻿namespace CsharpMyPortfolio.DAL.Entities
+{
+    public class Skill
+    {
+        public int SkillId { get; set; }
+        public string Title { get; set; }
+        public int value { get; set; }
+    }
+}
